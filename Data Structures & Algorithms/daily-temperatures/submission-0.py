f919625c893ba919186d95bd362858a531609a25
Merrick Neo <71166759+Merrickneo@users.noreply.github.com>
2026-09-18 23:05:00 +0800

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        '''
+        Monotonic stack is strictly increasing
+        
+        We need a way to track how many indexes ago was a smaller temperature was found
+        + have to store the temperature and the index
+
+        If we find a larger temperature as compared to the previously seen
+        elements then we will record down the diff in the indexes
+
+        monotonic stack: whereby the elements are strictly increasing temperatures. still in the stack means we have not matched with a future larger temperature
+
+        '''
+        n = len(temperatures)
+        output = [0] * n
+        stack = []
+        for index, temperature in enumerate(temperatures):
+            if stack:
+                # check if we can pop and record down the diff
+                while stack and stack[-1][0] < temperature:
+                    top, j = stack.pop()
+                    diff = index - j
+                    output[j] = diff
+            stack.append((temperature, index))
+        return output
+
+
+
+
+
+
+
+        
